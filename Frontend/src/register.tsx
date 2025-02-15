01ca0b70e242042c93b2793cf311d35b360b549e
Piotr Janiszek <pietrasos567@gmail.com>
2025-02-15 02:15:21 +0100

    Added account type to the registration form and upgraded style

--- Frontend/src/register.tsx
+++ Frontend/src/register.tsx
@@ -10,7 +10,8 @@ const register: Component = () => {
     const [lastName, setLastName] = createSignal("");
     const [username, setUsername] = createSignal("")
     const [phone, setPhone] = createSignal("")
-
+    type AccountType = 'employer' | 'jobSeeker' | 'freelancer';
+    const [accountType, setAccountType] = createSignal<AccountType>('jobSeeker');
 
     const handleSubmit = (event: Event) => {
         event.preventDefault();
@@ -30,8 +31,14 @@ const register: Component = () => {
                 <h1 class="text-center mb-4">Sign Up</h1>
 
                 <div class="mb-4">
-                    Username: {username()}
+                    <label
+                        for="accountType"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Username: {username()}
+                    </label>
                     <input
+                        id="username"
                         type="text"
                         name="username"
                         placeholder="Username"
@@ -44,8 +51,14 @@ const register: Component = () => {
                 </div>
 
                 <div class="mb-4">
-                    Email: {email()} {/* Wyświetlanie aktualnej wartości */}
+                    <label
+                        for="email"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Email: {email()} {/* Wyświetlanie aktualnej wartości */}
+                    </label>
                     <input
+                        id="email"
                         name="email"
                         type="email"
                         placeholder="Email"
@@ -58,8 +71,14 @@ const register: Component = () => {
                 </div>
 
                 <div class="mb-4">
-                    Password: {password()}
+                    <label
+                        for="password"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Password: {password()}
+                    </label>
                     <input
+                        id="password"
                         type="password"
                         name="password"
                         placeholder="Password"
@@ -73,8 +92,14 @@ const register: Component = () => {
                 </div>
 
                 <div class="mb-4">
-                    Confirm Password: {confirmPassword()}
+                    <label
+                        for="confirmpassword"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Confirm Password: {confirmPassword()}
+                    </label>
                     <input
+                        id="confirmpassword"
                         type="password"
                         name="confirmpassword"
                         placeholder="Confirm Password"
@@ -87,8 +112,14 @@ const register: Component = () => {
                 </div>
 
                 <div class="mb-4">
-                    First Name: {firstName()}
+                    <label
+                        for="firstname"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        First Name: {firstName()}
+                    </label>
                     <input
+                        id="firstname"
                         type="text"
                         name="firstname"
                         placeholder="First Name"
@@ -101,8 +132,14 @@ const register: Component = () => {
                 </div>
 
                 <div class="mb-4">
-                    Last Name: {lastName()}
+                    <label
+                        for="lastname"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Last Name: {lastName()}
+                    </label>
                     <input
+                        id="lastname"
                         type="text"
                         name="lastname"
                         placeholder="Last Name"
@@ -115,8 +152,14 @@ const register: Component = () => {
                 </div>
 
                 <div class="mb-4">
-                    Phone: {phone()}
+                    <label
+                        for="phone"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Phone: {phone()}
+                    </label>
                     <input
+                        id="phone"
                         type="tel"
                         name="phone"
                         placeholder="Phone Number"
@@ -127,6 +170,25 @@ const register: Component = () => {
                         class="border p-2 w-full"
                     />
                     <br/>
+                </div>
+
+                <div class="mb-4">
+                    <label
+                        for="accountType"
+                        class="text-sm font-medium text-gray-700 mb-2 dark:text-gray-300"
+                    >
+                        Typ konta
+                    </label>
+                    <select
+                        id="accountType"
+                        value={accountType()}
+                        onChange={(e) => setAccountType(e.currentTarget.value as AccountType)}
+                        class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white dark:bg-gray-800"
+                    >
+                        <option value="employer" >Pracodawca</option>
+                        <option value="jobSeeker">Poszukujący pracy</option>
+                        <option value="freelancer">Freelancer</option>
+                    </select>
                 </div>
 
                 <button type="submit" class="p-2 rounded shadow-md bg-gray-50 dark:bg-gray-800">Submit</button>
