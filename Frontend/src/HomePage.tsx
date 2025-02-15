45505466b71a7523fb4a3cd60f42e1c2dc70acc0
Piotr Janiszek <pietrasos567@gmail.com>
2025-02-15 02:47:12 +0100

    Added switching through offer types

--- Frontend/src/HomePage.tsx
+++ Frontend/src/HomePage.tsx
@@ -1,9 +1,11 @@
 import './HomePage.css'
-import {Component, onMount} from "solid-js";
+import {Component, createSignal, Match, onMount, Switch} from "solid-js";
 import * as L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const HomePage: Component = () => {
+    const [offerType, setOfferType] = createSignal(0);
+
     let map;
 
     function createMap() {
@@ -23,10 +25,45 @@ const HomePage: Component = () => {
     });
 
     return (
-        <div class="flex justify-end items-start min-h-screen w-full p-26">
-            <div id="map" class="h-150 w-250 z-0 relative shadow-md border-4 border-purple-300 rounded-md">
-
+        <div class="flex flex-col w-full">
+            <div class="flex gap-4 p-4 justify-center">
+                <button
+                    class={`px-4 py-2 rounded-md w-40 transition-colors ${
+                        offerType() === 0
+                            ? 'bg-purple-600 dark:bg-purple-500 text-white'
+                            : 'bg-purple-200 hover:bg-purple-300 dark:bg-purple-900 dark:hover:bg-purple-800 dark:text-purple-100'
+                    }`}
+                    onClick={() => {setOfferType(0); createMap()}}
+                >
+                    Oferty pracy
+                </button>
+                <button
+                    class={`px-4 py-2 rounded-md w-40 transition-colors ${
+                        offerType() === 1
+                            ? 'bg-purple-600 dark:bg-purple-500 text-white'
+                            : 'bg-purple-200 hover:bg-purple-300 dark:bg-purple-900 dark:hover:bg-purple-800 dark:text-purple-100'
+                    }`}
+                    onClick={() => setOfferType(1)}
+                >
+                    Usługi freenlancer
+                </button>
             </div>
+
+
+            <Switch>
+                <Match when={offerType() === 0}>
+                    <div class="flex justify-end items-start min-h-screen w-full p-26">
+                        <div id="map" class="h-150 w-250 z-0 relative shadow-md border-4 border-purple-300 rounded-md">
+
+                        </div>
+                    </div>
+                </Match>
+                <Match when={offerType() === 1}>
+                    <div class="flex justify-center items-start min-h-screen w-full p-26">
+                        Oferta 1
+                    </div>
+                </Match>
+            </Switch>
         </div>
     );
 };
